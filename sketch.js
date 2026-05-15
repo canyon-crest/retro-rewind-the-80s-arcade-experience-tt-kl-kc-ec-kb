98de8ace01fb15e64c8fa6e08ae82fecb6d8f9ac
emily4187 <chene2676@my.sduhsd.net>
2026-05-15 22:28:16 +0000

    added moving blocks

--- sketch.js
+++ sketch.js
@@ -7,10 +7,22 @@ tralalero.y = -50;
 tralalero.width = 20; 
 tralalero.height = 20; 
 tralalero.rotationLock = true;
-
 tralalero.scale = 0.2;
 tralalero.img = 'sprites/tralalero.png'; 
 tralalero.img.offset.y = -210; 
+
+let tung = new Sprite();
+tung.x = 100;
+tung.y = -50;
+tung.width = 20;
+tung.height = 20;
+tung.rotationLock = true;
+tung.scale = 1.5;
+tung.gif = 'sprites/ttts.gif';
+
+let jumpCount = 0;
+const maxJumps = 2;
+const jumpVel = -7;
 
 let groundA = new Sprite();
 groundA.x = -80;
@@ -39,6 +51,19 @@ groundC.rotation = 0;
 groundC.physics = 'static';
 groundC.color = 'yellow';
 
+let patroller = new Sprite();
+patroller.x = -80;            
+patroller.y = 50;             
+patroller.width = 20;
+patroller.height = 20;
+patroller.color = 'red';
+patroller.rotationLock = true;
+patroller.physics = 'kinematic'; 
+patroller.vel.x = 3;          
+
+let groundLeftEdge = groundA.x - (groundA.width / 2);  
+let groundRightEdge = groundA.x + (groundA.width / 2); 
+
 q5.update = function () {
 	background('skyblue');
 
@@ -50,5 +75,18 @@ q5.update = function () {
 	else if (kb.pressing('right')) tralalero.vel.x = 7;
 	else tralalero.vel.x = 0;
 	
-	if (kb.presses('up')) tralalero.vel.y = -7;
+	if ((mouse.presses() || kb.presses('space') || kb.presses('up')) && jumpCount < maxJumps) {
+		tralalero.vel.y = jumpVel;
+		jumpCount++;
+	}
+
+	if (tralalero.collide(groundA) || tralalero.collide(groundB) || tralalero.collide(groundC)) {
+		jumpCount = 0;
+	}
+
+	if (patroller.x >= groundRightEdge - (patroller.width / 2)) {
+		patroller.vel.x = -3; 
+	} else if (patroller.x <= groundLeftEdge + (patroller.width / 2)) {
+		patroller.vel.x = 3;
+	}
 };
